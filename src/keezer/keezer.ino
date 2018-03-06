f7adf85331f36837c8c57ccbf11af99877012dab
Mike Linnen <mlinnen@protosystem.net>
2018-03-05 23:34:24 -0500

    Updated config settings and added some comments

--- src/keezer/keezer.ino
+++ src/keezer/keezer.ino
@@ -12,7 +12,10 @@ void setup()
   delay(5000);
 
   // Initialize the temperature controller
-  tempController = new TemperatureController(12, TEMPERATURE_LOW, TEMPERATURE_HIGH);
+  tempController = new TemperatureController(
+    12, 
+    DEFAULT_TEMPERATURE_SETPOINT_LOW, 
+    DEFAULT_TEMPERATURE_SETPOINT_HIGH);
 
   // Execute the setup on the temperature controller
   tempController->setup();
@@ -20,5 +23,10 @@ void setup()
 
 void loop()
 {
+  // TODO Look for any messages that came in over MQTT
+
+  // Process all the loops
   tempController->loop();
+
+  // TODO Determine if any state changes or sensor changes need to be broadcasted out over MQTT
 }
