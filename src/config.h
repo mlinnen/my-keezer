cfbece2d70805211a2556f722ba4dd79ef6225e9
Mike Linnen <mlinnen@protosystem.net>
2018-12-04 23:20:06 -0500

    Convert to Platform IO

---
+++ src/config.h
@@ -0,0 +1,4 @@
+/************************* Temperature Controller *********************************/
+
+#define DEFAULT_TEMPERATURE_SETPOINT_HIGH 40.0
+#define DEFAULT_TEMPERATURE_SETPOINT_LOW  38.0
