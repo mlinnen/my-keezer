f489d9275b049c125063ffea93798177153d559a
Mike Linnen <mlinnen@protosystem.net>
2018-12-07 17:52:43 -0500

    copy the config.sample.h to config.h

--- src/config.h
+++
@@ -1,4 +0,0 @@
-/************************* Temperature Controller *********************************/
-
-#define DEFAULT_TEMPERATURE_SETPOINT_HIGH 40.0
-#define DEFAULT_TEMPERATURE_SETPOINT_LOW  38.0
