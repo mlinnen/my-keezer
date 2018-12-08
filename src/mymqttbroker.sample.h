3299551cf050b65347a27c8ef36beb9e9d11c009
Mike Linnen <mlinnen@protosystem.net>
2018-12-08 14:03:14 -0500

    Changed the topics

--- src/mymqttbroker.sample.h
+++ src/mymqttbroker.sample.h
@@ -5,6 +5,6 @@
 #define MQTT_USERNAME    "username"             // The user name for the MQTT broker
 #define MQTT_PASSWORD    "password"             // The user name's password for the MQTT broker
 
-#define MQTT_TOPIC_PING  "/ha/keezer/ping"      // The topic that this devices listens for
-#define MQTT_TOPIC_PINGR "/ha/keezer/pingr/1"   // The topic that this devices publishes as a response to a ping request
+#define MQTT_TOPIC_PING  "/home/keezer/ping"    // The topic that all keezer devices listens to
+#define MQTT_TOPIC_PINGR "/home/keezer/pingr/1" // The topic that this devices publishes as a response to a ping request
                                                 // The 1 at the end of the topic is the deviceId in case you have more than 1 keezer
