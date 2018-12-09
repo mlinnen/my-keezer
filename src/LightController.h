d27dd12c257bfb91ce587258b5c6fe0f8c93d902
Mike Linnen <mlinnen@protosystem.net>
2018-12-08 21:47:26 -0500

    Publish motion value to MQTT

--- src/LightController.h
+++ src/LightController.h
@@ -3,17 +3,22 @@
 
 #include "Arduino.h"
 #include <RBD_Timer.h>
-
+#include <PubSubClient.h>
+#include "mymqttbroker.h"
+#include "config.h"
 
 class LightController
 {
   public:
-    LightController(int motionSensorPin,int lightRelayPin);
+    LightController(int motionSensorPin,int lightRelayPin, PubSubClient &client);
     void setup();
     boolean loop();
   private:
     int _lightRelayPin;
     int _motionSensorPin;
+    PubSubClient _client;
+    void publishMotion(const char* topic, boolean motion);
+    boolean _lastMotion;
 };
 
 #endif
