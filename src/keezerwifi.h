73cf926bbe65896a249de45903df47c6cc98188b
Mike Linnen <mlinnen@protosystem.net>
2019-01-01 19:28:59 -0500

    Fixed a bug where MQTT settings were not saved after manual wifi setup

--- src/keezerwifi.h
+++ src/keezerwifi.h
@@ -7,8 +7,9 @@
 #include <WiFiManager.h>
 #include <RBD_Timer.h>
 
+boolean keezerwifi_saveConfig();
 void keezerwifi_setup(Config &config, boolean manualConnect);
 void keezerwifi_loop(Config &config);
-
+void keezerwifi_saveConfigCallback();
 
 #endif
